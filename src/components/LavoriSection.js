6aeefaf52971045c2105ef61959b31c4514f40a0
PeterQuadrini <quadrinipietro@gmail.com>
2024-07-01 17:10:50 +0200

    commit

--- src/components/LavoriSection.js
+++ src/components/LavoriSection.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles/LavoriSection.css'
 import Masonry from 'react-masonry-css';
 import { Fade } from "react-awesome-reveal";
+import { Skeleton } from '@chakra-ui/react';
 
 function LavoriSection(props) {
 
@@ -12,16 +13,35 @@ function LavoriSection(props) {
     500: 1
   };
 
+  console.log('All images loaded!');
+  const [loadedImagesCount, setLoadedImagesCount] = useState(0);
+  const [loaded, setLoaded] = useState(false);
+  const totalImages = props.data.length;
+
+  useEffect(() => {
+    if (loadedImagesCount === totalImages) {
+      setLoaded(true)
+      // Esegui altre azioni quando tutte le immagini sono caricate
+    }
+  }, [loadedImagesCount, totalImages]);
+
+  const handleImageLoad = () => {
+    console.log('okkk')
+    setLoadedImagesCount((prevCount) => prevCount + 1);
+  };
+
   return (
     <div className='LavoriSection'>
       <Masonry breakpointCols={breakpointColumnsObj} className="my-masonry-grid" columnClassName="my-masonry-grid_column">
         {props.data.map(data => <Fade key={data.file}>
-          <div className='containerLS' onClick={() => {props.onClicked(data.file)}}>
-            <div className='blackLS'>
-              <div className='titolo'>{data.titolo}</div>
+          <Skeleton isLoaded={loaded}>
+            <div className='containerLS' onClick={() => {props.onClicked(data.file)}}>
+              <div className='blackLS'>
+                <div className='titolo'>{data.titolo}</div>
+              </div>
+              <img className='image' src={data.file} onLoad={handleImageLoad}></img>
             </div>
-            <img className='image' src={data.file}></img>
-          </div>
+          </Skeleton>
         </Fade>)}
       </Masonry>
     </div>
